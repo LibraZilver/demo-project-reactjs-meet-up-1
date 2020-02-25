857c8680326461d197a78633e139922eabf1aef9
Later Never <Watchara.che@gmail.com>
2020-02-25 20:44:32 +0700

    add component in login page

--- src/App/Routes/Routes.tsx
+++ src/App/Routes/Routes.tsx
@@ -2,8 +2,12 @@ import React, { lazy, FC, Suspense } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import PrivateRoute from 'common/components/PrivateRoute'
 const FaceRegister = lazy(() => import('features/faceRegister/Routes'))
+const Authentication = lazy(() => import('features/authentication/Routes'))
 
-const routes = [{ restricted: false, path: '/face-register', Component: FaceRegister }]
+const routes = [
+  { restricted: false, path: '/face-register', Component: FaceRegister },
+  { restricted: false, path: '/auth', Component: Authentication },
+]
 
 const Routes: FC = () => (
   <Suspense fallback={<div />}>
--- src/features/authentication/Routes/Login/Login.tsx
+++ src/features/authentication/Routes/Login/Login.tsx
@@ -1,6 +1,16 @@
 import React, { FC } from 'react'
+import Header from 'common/components/Header'
 import './Login.scss'
 
-const Login: FC = () => <div className="Login">Login</div>
+const Login: FC = () => (
+  <div className="Login">
+    <Header />
+    <div className="title">Login</div>
+    <div className="text">username</div>
+    <input type="text" />
+    <div className="text">password</div>
+    <input type="password" />
+  </div>
+)
 
 export default Login
